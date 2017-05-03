a0ee85772ebd62aa5026f302e61ed775a38e4023
yapweiyih <heavenode@gmail.com>
2017-05-03 21:16:29 +0800

    Added documentation and output format

--- impact_hazedata.R
+++ impact_hazedata.R
@@ -1,7 +1,9 @@
 library(rvest)
 
+# NEA URL with key
 path <- "http://api.nea.gov.sg/api/WebAPI/?dataset=pm2.5_update&keyref=***REMOVED***"
 
+# read the data using rvest package
 out <- rvest::html(path)
 out
 
@@ -14,11 +16,25 @@ latitude
 longitude <- out %>% html_nodes("longitude") %>% html_text
 longitude
 
+# Read the XML attribute "timestamp" from Node "Record"
 timestamp  <- out %>% html_nodes("record") %>% html_attr("timestamp")
 timestamp
 
+# Read the XML attribute "value" from Node "reading"
 reading  <- out %>% html_nodes("reading") %>% html_attr("value")
 reading 
 
+# store the required info into a dataframe
 df <- data.frame(region,latitude,longitude,reading, timestamp)
 df
+
+# Todo
+# timestamp can be formatted is needed
+
+# output format
+#region latitude longitude reading      timestamp
+#1    rNO  1.41803 103.82000      33 20170503210000
+#2    rCE  1.35735 103.82000      23 20170503210000
+#3    rEA  1.35735 103.94000      15 20170503210000
+#4    rWE  1.35735 103.70000      26 20170503210000
+#5    rSO  1.29587 103.82000      31 20170503210000
