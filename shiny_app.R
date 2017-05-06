a21b1d0cdae379d7c371619e346b2e5f96530c9a
Mathew Josey <a0103328@nus.edu.sg>
2017-05-06 13:02:10 +0800

    Adds comments, beautifies plot, option to select regions for time-seris plot

--- shiny_app.R
+++ shiny_app.R
@@ -20,11 +20,14 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       radioButtons("type", "Select variable:", c("PSI", "PM25")),
-      uiOutput("ui_t_range"),
+      uiOutput("ui_t_range"), # Slider with time range that is updated when new readings
       selectInput("map_type", "Select map type:", 
                   c(paste0("google:", c("terrain", "satellite", "roadmap", "hybrid")),
                     paste0("stamen:", c("watercolor", "toner")))
-                  )
+                  ),
+      checkboxGroupInput("region", "Select regions to plot",
+                         choices = c("rNO", "rCE", "rEA", "rWE", "rSO"),
+                         selected = c("rNO", "rCE", "rEA", "rWE", "rSO"))
       
     ),
     
@@ -32,7 +35,7 @@ ui <- fluidPage(
     mainPanel(
       plotOutput("plot_map"),
       plotOutput("plot_ts"),
-      textOutput("txt_update_info")
+      textOutput("txt_update_info") # Shows the timestamp of latest reading available
     )
   )
 )
@@ -40,17 +43,17 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   
-  # check_timer = reactiveTimer(1 * 60 * 60 * 1000)
+  # Helper function to calculate the time to next hour:00:00 + 5mins
   ms_to_next_update = function() (60 + 5 - minute(Sys.time()))*60*1000
   
+  # Updates the local data base and sets text output to latest time stamp
   output$txt_update_info = reactive({
-    # check_timer()
     invalidateLater(ms_to_next_update(), session)
     paste("Last updated reading from:", as.character(update_hist_reading()))
   })
   
+  # Updates the time slider
   output$ui_t_range = renderUI({
-    # check_timer()
     invalidateLater(ms_to_next_update(), session)
     mi = min(hist_df$timestamp)
     ma = max(hist_df$timestamp)
@@ -58,6 +61,7 @@ server <- function(input, output, session) {
                 mi, ma, value = c(mi, ma))
   })
   
+  # Gets latest reading and updates the map with given type and variable
   output$plot_map = renderPlot({
     sg_map = get_SG_map(input$map_type)
     plotDF = get_latest_reading()
@@ -69,11 +73,15 @@ server <- function(input, output, session) {
       geom_label(data=plotDF, aes(longitude, latitude, 
                                   label=Value, col=Classification), size=10) +
       scale_color_manual(values = haze_color_map) +
-      theme(legend.position = "")
+      theme(legend.position = "")+
+      ggtitle(paste(input$type, "values recorded at", max(plotDF$timestamp)))
   })
   
+  # Generates the time-series plot considering in the selected range and regions
   output$plot_ts = renderPlot({
-    plotDF = hist_df[timestamp %between% input$t_range]
+    if (!length(input$region))
+      return(ggplot())
+    plotDF = hist_df[(timestamp %between% input$t_range) & (region %in% c(input$region))]
     plotDF$Value = switch(input$type,
                           "PSI" = round(pm25_to_psi(plotDF$reading)),
                           "PM25" = plotDF$reading)
@@ -81,7 +89,8 @@ server <- function(input, output, session) {
       geom_point(aes(shape = region))+
       geom_line()+
       ylab(input$type)+
-      theme_light()
+      theme_light()+
+      ggtitle(paste("Historical trend of", input$type, "for selected region and time range"))
   })
 }
 
--- shiny_helper_functions.R
+++ shiny_helper_functions.R
@@ -2,26 +2,37 @@
 library(ggmap)
 library(rvest)
 library(data.table)
-library(fasttime)
 
-# Parameter settings
+# Parameter settings ------------------------------------------------------
+
+# The main file to store/read the values from
+hist_file_name = "hist_readings.csv"
+# Data frame with the range of values for different categories
 category_df = data.frame(pm25.low = c(0, 13, 56, 151, 251, 351),
                          pm25.high = c(12, 55, 150, 250, 350, 500) + 1, # Should be closed range
                          psi.low = c(0, 51, 101, 201, 301, 401),
                          psi.high = c(50, 100, 200, 300, 400, 500) + 1, # Should be closed range
                          category = c("Good", "Moderate", "Unhealthy", "Very unhealthy", "Hazardous", "Hazardous"))
+# Map from the categories to the color to be used to print the label
 haze_color_map = c("Good"="green","Moderate"="blue","Unhealthy"="yellow","Very unhealthy"="orange","Hazardous"="red")
+# Variable to hold the maps downloaded using get_map. Dynamic storage prevents delay while refreshing
 map_list = list()
-hist_file_name = "hist_readings.csv"
+
+
+# Historical update -------------------------------------------------------
+
+# Creates the hist_file_name if not found
 if (! file.exists(hist_file_name)) {
   cat("Creating new historical readings data base.")
   writeLines("region,latitude,longitude,reading,timestamp", hist_file_name)
 }
+# Reads the previously stored values
 hist_df = fread(hist_file_name, header = TRUE,
                 colClasses = c("factor", "numeric", "numeric", "numeric", "character"))
-hist_df[, timestamp := as.POSIXct(timestamp, format = "%Y-%m-%d %H:%M:%S")]
+hist_df[, timestamp := as.POSIXct(timestamp, format = "%Y%m%d%H%M%S")]
 
-# Conversion functions
+
+# Conversion functions (PSI<>PM) ------------------------------------------
 
 pm25_to_psi = function(pm_values) {
   ix = index_in_range(pm_values, "PM25")
@@ -31,23 +42,9 @@ pm25_to_psi = function(pm_values) {
     category_df$psi.low[ix]
 }
 
-# Properties functions
+# Map functions -----------------------------------------------------------
 
-get_category = function(values, type) {
-  ix = index_in_range(values, type)
-  category_df$category[ix]
-}
-
-# Helper functions
-
-index_in_range = function(value, type) {
-  r = switch(type, 
-             "PSI" = category_df[, c("psi.low", "psi.high")],
-             "PM25" = category_df[, c("pm25.low", "pm25.high")])
-  sapply(value, function(v) which(v>=r[, 1] & v<r[, 2]))
-}
-
-# Map functions
+# Download map if not previously done. Return the stored map
 get_SG_map = function(map_type) {
   if ( !(map_type %in% names(map_list)) ) {
     sp = strsplit(map_type, ":")[[1]]
@@ -58,15 +55,16 @@ get_SG_map = function(map_type) {
   map_list[[map_type]]
 }
 
-# xml parsing
-get_latest_reading = function() {
-  hist_df[timestamp == max(timestamp), .SD, by = region]
-}
+# NEA scrapping functiions ------------------------------------------------
 
+# Updates hist_df and hist_file_name if new readings are obtained. Returns the latest timestamp in hist_df
 update_hist_reading = function() {
   path <- "http://api.nea.gov.sg/api/WebAPI/?dataset=pm2.5_update&keyref=***REMOVED***"
-  # read the data using rvest package
-  out <- read_html(path)
+  out <- read_html(path) # read the data using rvest package
+  # # Un comment followin lines for later debugging
+  # f = file("log.txt", "a")
+  # writeLines(c(as.character(Sys.time()), as.character(out)), f)
+  # close(f)
   region <- out %>% html_nodes("id") %>% html_text
   latitude <- as.numeric(out %>% html_nodes("latitude") %>% html_text)
   longitude <- as.numeric(out %>% html_nodes("longitude") %>% html_text)
@@ -74,9 +72,33 @@ update_hist_reading = function() {
                            format = "%Y%m%d%H%M%S")
   reading  <- as.numeric( out %>% html_nodes("reading") %>% html_attr("value") )
   df <- data.table(region, latitude, longitude, reading, timestamp)
-  # Find if does not exist in hist_df and add
+  
+  # Find if readings do not exist in hist_df and add
   found = tail(duplicated(rbind(hist_df, df)), nrow(df))
   hist_df <<- rbind(hist_df, df[!found, ])
+  df[, timestamp := as.character(timestamp, format = "%Y%m%d%H%M%S")]
   write.table(x=df[!found, ], file=hist_file_name, append = TRUE, sep =",", row.names = FALSE, col.names = FALSE)
   max(hist_df$timestamp)
+}
+
+# Historical Extraction Functions -----------------------------------------
+
+get_latest_reading = function() {
+  hist_df[timestamp == max(timestamp), .SD, by = region]
+}
+
+# Helper functions --------------------------------------------------------
+
+# Extract the category for each value
+get_category = function(values, type) {
+  ix = index_in_range(values, type)
+  category_df$category[ix]
+}
+
+# Convinence function to find index in category_df
+index_in_range = function(value, type) {
+  r = switch(type, 
+             "PSI" = category_df[, c("psi.low", "psi.high")],
+             "PM25" = category_df[, c("pm25.low", "pm25.high")])
+  sapply(value, function(v) which(v>=r[, 1] & v<r[, 2]))
 }
