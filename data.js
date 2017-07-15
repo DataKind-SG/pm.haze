2b7526cd3d4f9909fe6f7f55f5b64d2fa613f76d
raymond.chan.wai.mun@gmail.com <raymond.chan.wai.mun@gmail.com>
2017-07-15 14:59:02 +0800

    Sample data for PSI time series added.

---
+++ data.js
@@ -0,0 +1,502 @@
+[
+	{
+		region: "north",
+		timestamp: [
+			{
+				hour: 5,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 6,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 7,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 8,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 9,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 10,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 11,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 12,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 13,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 14,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 15,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 16,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 17,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 18,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 19,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 20,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 21,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 22,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 23,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 24,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 1,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			},
+			{
+				hour: 2,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			},
+			{
+				hour: 3,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			},
+			{
+				hour: 4,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			}
+		]
+	},
+	{
+		region: "south",
+		timestamp: [
+			{
+				hour: 5,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 6,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 7,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 8,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 9,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 10,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 11,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 12,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 13,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 14,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 15,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 16,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 17,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 18,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 19,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 20,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 21,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 22,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 23,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 24,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 1,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			},
+			{
+				hour: 2,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			},
+			{
+				hour: 3,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			},
+			{
+				hour: 4,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			}
+		]
+	},
+	{
+		region: "east",
+		timestamp: [
+			{
+				hour: 5,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 6,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 7,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 8,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 9,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 10,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 11,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 12,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 13,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 14,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 15,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 16,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 17,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 18,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 19,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 20,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 21,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 22,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 23,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 24,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 1,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			},
+			{
+				hour: 2,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			},
+			{
+				hour: 3,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			},
+			{
+				hour: 4,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			}
+		]
+	},
+	{
+		region: "west",
+		timestamp: [
+			{
+				hour: 5,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 6,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 7,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 8,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 9,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 10,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 11,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 12,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 13,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 14,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 15,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 16,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 17,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 18,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 19,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 20,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 21,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 22,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 23,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 24,
+				date: "2017-07-07",
+				PSIvalue: 123,
+			},
+			{
+				hour: 1,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			},
+			{
+				hour: 2,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			},
+			{
+				hour: 3,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			},
+			{
+				hour: 4,
+				date: "2017-07-08",
+				PSIvalue: 123,
+			}
+		]
+	}
+]
